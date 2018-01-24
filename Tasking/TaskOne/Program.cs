fa2333b54df9b1d11f2f184e92a7763f4db653e8
UncleAlexKun <raisin27@mail.ru>
2018-01-24 23:47:45 +0400

    Запись в файл

--- Tasking/TaskOne/Program.cs
+++ Tasking/TaskOne/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace TaskOne
 	{
 		static void Main(string[] args)
 		{
+			using (var reader = new StreamReader("input.txt"))
+			{
+				
+			}
+
+			using (var writer = new StreamWriter("output.txt"))
+			{
+
+			}
 		}
 	}
 }
